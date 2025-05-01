069913c07f8470d6739c12467c82aea50330869c
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-05-01 14:27:39 +0200

    login

--- index.js
+++ index.js
@@ -2,24 +2,70 @@ import express from "express"
 import 'dotenv/config'
 import jwt from 'jsonwebtoken'
 
+//TEST
+// #FIXME do real db
+const users = [
+    {
+        username: "a",
+        password: "a",
+        mail: "a@example.com",
+    },
+    {
+        username: "b",
+        password: "b",
+        mail: "b@example.com",
+    },
+    {
+        username: "c",
+        password: "c",
+        mail: "c@example.com",
+    }
+]
 const app = express()
 app.use(express.json())
 
 app.post('/auth/login', (req, res) => {
-//find user in db
-//jwt sign
-//refresh token on cookie
-//send jwt
+    const { username, password, mail } = req.body
+    if (!(username && password && mail)) return res.sendStatus(401)
+
+    const user = users.filter((u) =>
+        u.username === username &&
+        u.password === password &&
+        u.mail === mail)[0]
+    if (!user) return res.sendStatus(401)
+
+    const authToken = jwt.sign(
+        { username, mail },
+        process.env.AUTH_TOKEN_KEY,
+        {
+            expiresIn: '15m'
+        }
+    )
+    const refreshToken = jwt.sign(
+        { username, mail },
+        process.env.REFRESH_TOKEN_KEY,
+        {
+            expiresIn: '1d'
+        }
+    )
+
+    res.cookie('jwt', refreshToken, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 //1day
+    })
+
+    res.json(authToken)
+
 })
 
 app.post('/auth/logout', (req, res) => {
-//add tokens to ban list
+    //add tokens to ban list
 })
 
 app.post('/auth/refresh', (req, res) => {
-//get refresh token
-//jwt sign
-//send jwt
+    //get refresh token
+    //jwt sign
+    //send jwt
 })
 
 app.listen(process.env.EXPRESS_PORT || 3200, () => {
