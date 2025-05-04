aeb6e1c0200c2571705640caca8464e63aad7785
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-05-04 17:01:25 +0200

    some model validation

--- models/auth.js
+++ models/auth.js
@@ -5,9 +5,15 @@ mongoose.connect('mongodb://localhost/users')
     .catch(err => console.error(err))
 
 const UserSchema = new Schema({
-    mail: { type: String, unique: true, required: true },
+    mail: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        match: /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/
+    },
     password: { type: String, required: true },
-    role: { type: String, default: "user" }
+    role: { type: String, default: "user", enum: ["user", "admin"] }
 })
 
 export const User = mongoose.model('User', UserSchema)
