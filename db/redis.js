854bef91f446187090e2467ea9758db7de2b19c5
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-05-29 14:48:14 +0200

    logging tool

--- db/redis.js
+++ db/redis.js
@@ -1,6 +1,7 @@
 import {createClient} from "redis"
+import { logger } from "../winston.js";
 
 export const redisClient = await createClient()
-  .on("error", (err) => console.log("Redis Client Error", err))
-  .on('connect',()=>console.log("redis connected"))
+  .on("error", (err) => logger.error("Redis Client Error", err))
+  .on('connect',()=>logger.info("redis connected"))
   .connect();
