eb2bf95cfdd8349d8e8e67123fb994755594952a
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-06-14 17:37:45 +0200

    lint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+]);
