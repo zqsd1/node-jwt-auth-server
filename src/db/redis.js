b4bfdd9344854be541d47fe2bffa656a669a7342
zqsd <17475349+zqsd1@users.noreply.github.com>
2025-07-06 15:58:27 +0200

    variable for redis url

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,7 +1,7 @@
-import {createClient} from "redis"
+import { createClient } from "redis"
 import { logger } from "../winston.js";
 
-export const redisClient = await createClient()
+export const redisClient = await createClient({ url: process.env.REDIS_URL })
   .on("error", (err) => logger.error("Redis Client Error", err))
-  .on('connect',()=>logger.info("redis connected"))
+  .on('connect', () => logger.info("redis connected"))
   .connect();
